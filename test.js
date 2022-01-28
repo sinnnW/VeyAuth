8ca2f53e671e63e17b439c0ce6fe8ce1444525d6
verlox <verlox@riseup.net>
2022-01-28 06:00:44 -0500

    update logging format

--- test.js
+++ test.js
@@ -6,7 +6,12 @@ require("dotenv").config();
 
 var auth = new Auth({
     level: 'debug',
-    format: winston.format.json(),
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        winston.format.align(),
+        winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    ),
     transports: [
         new winston.transports.Console({ format: winston.format.colorize() }),
         new winston.transports.File({ filename: 'debug.log', level: 'debug' }),
@@ -17,13 +22,13 @@ var auth = new Auth({
 
 setTimeout(async() => {
     try {
-        var admin = await User.get('token');console.log(admin)
+        var admin = await User.get('token');
         var app = await App.get(-1);
-        User.create(admin, app, 'verlox', 'testpass')
+        User.create(admin, app, 'verlox2', 'testpass')
             .then(usr => {
                 // User.get(0).then(console.log).catch(console.error);
-                usr.application.owner = "REMOVED FOR OUTPUT"
-                fs.writeFileSync(__dirname + 'user.txt', JSON.stringify(usr, null, 2));
+                // usr.application.owner = "REMOVED FOR OUTPUT"
+                // fs.writeFileSync(__dirname + 'user.txt', JSON.stringify(usr, null, 2));
             })
             .catch(console.error);
     }catch (e)
