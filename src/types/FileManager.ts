18ff63011ed01bbc00e7ebf657bda9509425dd78
verlox <verlox@riseup.net>
2022-04-02 17:35:41 -0400

    add comments

--- src/types/FileManager.ts
+++ src/types/FileManager.ts
@@ -14,20 +14,25 @@ export class FileManager implements IFileManager {
     this.#auth = auth;
   }
 
+  /**
+   * This will update the cached data
+   */
   _getData(): Promise<void> {
     return new Promise<void>(async (resolve, _) => {
       let files = await File.getAll(this.#auth); 
-
-      // console.trace(files)
-
       this.all = files;
-      // this.user = files.filter(f => f.user != null);
-
       resolve();
     })
   }
 
-  create(fileName: string, data: any, priv?: boolean): Promise<File> {
+  /**
+   * Create a new file
+   * @param {string} fileName
+   * @param {string} data
+   * @param {boolean} priv
+   * @returns {Promise<File>} File created
+   */
+  create(fileName: string, data: string, priv?: boolean): Promise<File> {
     return new Promise<File>(async (resolve, _) => {
       let f = await File.create(this.#auth, this.#auth.application, this.#auth, fileName, data, priv);
       await this._getData();
