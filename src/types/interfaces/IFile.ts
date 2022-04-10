272fef3882e5a04d66f13598f0337baaa5aad7fc
verlox <verlox@riseup.net>
2022-04-09 20:25:32 -0400

    implement interface

--- src/types/interfaces/IFile.ts
+++ src/types/interfaces/IFile.ts
@@ -1,5 +1,16 @@
 import { IBase } from './IBase';
+import { User } from '../User'; 
 
 export interface IFile extends IBase {
+  format: string;
+  data: string;
 
+  rename(name: string): void;
+  setPrivate(priv: boolean): void;
+  setDisabled(disabled: boolean): void;
+  enable(): void;
+  disable(): void;
+  setDisableReason(reason: string): void;
+  save(auth: User): Promise<IFile>;
+  delete(auth: User): Promise<void>;
 }
