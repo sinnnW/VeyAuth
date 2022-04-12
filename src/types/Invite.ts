0a849b79b0ebc9c94b179f673f96946f1544a2d4
verlox <verlox@riseup.net>
2022-04-11 22:24:17 -0400

    add: code comments + static delete() method

--- src/types/Invite.ts
+++ src/types/Invite.ts
@@ -14,16 +14,38 @@ export class Invite implements IInvite {
 
   #changes = false;
   #deleted = false;
+  #claimed = false;
 
+  /**
+   * Format the invite's data
+   */
   get format(): string {
     return `([Code ${this.code.split('-').slice(0, this.code.split('-').length - 1).join('-').replace(/[A-Z0-9]/gi, '#')}-${this.code.split('-').slice(this.code.split('-').length - 1, this.code.split('-').length).toString()}] [Inviter: ${this.createdBy.format}] ${this.claimedBy ? `[Claimer: ${this.claimedBy?.format}])` : `[Unclaimed]`}`;
   }
 
+  /**
+   * Claim the current invite
+   * @param {User} user The user claiming the code
+   * @returns {Promise<void>}
+   */
+  claim(user: User): Promise<void> {
+    return Invite.claim(this.application, user, this.code);
+  }
+
+  /**
+   * Set invite expiration
+   * @param {Date} expires Set expiration of the invite
+   */
   setExpiration(expires: Date) {
     this.expires = expires;
     this.#changes = true;
   }
 
+  /**
+   * Save all changes to the invite
+   * @param {User} auth Authorization
+   * @returns {Promise<Invite>} Updated invitation
+   */
   save(auth: User): Promise<Invite> {
     return new Promise<Invite>((resolve, reject) => {
       if (this.#deleted)
@@ -44,7 +66,7 @@ export class Invite implements IInvite {
       // Run all the save commands
       Core.logger.debug(`Saving invite information for ${this.format}, auth: ${auth.format}`);
       Core.db.serialize(() => {
-        // Update disable_reason
+        // Update expires_at
         Core.db.run('UPDATE invites SET expires_at = ? WHERE application_id = ? AND code = ?', [this.expires.getTime() / 1000, this.application.id, this.code], async () => {
           Core.logger.debug('Updated disable_reason');
 
@@ -59,26 +81,59 @@ export class Invite implements IInvite {
     });
   }
   
+  /**
+   * Delete the invite
+   * @param {User} auth Authorization
+   * @returns {Promise<void>}
+   */
   delete(auth: User): Promise<void> {
-    return new Promise((resolve, reject) => {
-      if (!auth?.permissions.has(FLAGS.DELETE_INVITES, this.application.id))
+    this.#deleted = true;
+    return Invite.delete(auth, this.application, this.code);
+  }
+
+  static delete(auth: User, app: App, code: string): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      let inv = await Invite.get(app, code);
+      
+      if (!auth?.permissions.has(FLAGS.DELETE_INVITES, app.id) && auth?.id != inv.createdBy.id)
         return reject('Invalid permissions');
 
-      Core.db.run('DELETE FROM invites WHERE application_id = ? AND code = ?', [this.application.id, this.code], err => {
+      Core.db.run('DELETE FROM invites WHERE application_id = ? AND code = ?', [app.id, code], err => {
         if (err)
           return reject(err);
-
-        this.#deleted = true;
 
         resolve();
       })
     })
   }
 
-  static claim(app: App, code: string, user: User) {
-    Core.db.run('UPDATE invites SET claimed_by = ? WHERE application_id = ? AND code = ?', [user.id, app.id, code]);
+  /**
+   * Claim an invite
+   * @param {App} app Application
+   * @param {User} user User claiming
+   * @param {string} code Invite code
+   * @returns {Promise<void>} 
+   */
+  static claim(app: App, user: User, code: string): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      let inv = await Invite.get(app, code); 
+      if (inv.claimedBy != null)
+        return reject('Invite has already been claimed');
+      
+      Core.db.run('UPDATE invites SET claimed_by = ? WHERE application_id = ? AND code = ?', [user.id, app.id, code], err => {
+        if (err)
+          return reject(err)
+        
+        resolve();
+      });
+    })
   }
 
+  /**
+   * Generate an invite code
+   * @param {App} app Application
+   * @returns {Promise<string>} Generated code
+   */
   static #generateCode(app: App): Promise<string> {
     return new Promise<string>((resolve, reject) => {
       let codeSegments = [];
@@ -98,6 +153,12 @@ export class Invite implements IInvite {
     })
   }
 
+  /**
+   * Get an invite by code
+   * @param {App} app Application
+   * @param {string} code Invite code 
+   * @returns {Promise<Invite>} Invite found
+   */
   static get(app: App, code: string): Promise<Invite> {
     return new Promise<Invite>((resolve, reject) => {
       Core.db.get('SELECT * FROM invites WHERE application_id = ? AND code = ?', [app.id, code], async (err, data) => {
@@ -111,6 +172,14 @@ export class Invite implements IInvite {
     })
   }
 
+  /**
+   * Create a new invite
+   * @param {User} auth Authorization
+   * @param {App} app Application 
+   * @param {User} user User to create under
+   * @param {Date} expires Expiration 
+   * @returns {Promise<Invite>} Invite created
+   */
   static create(auth: User, app: App, user?: User, expires?: Date): Promise<Invite> {
     return new Promise<Invite>(async (resolve, reject) => {
       if (!auth?.permissions.has(FLAGS.CREATE_INVITES, app.id))
@@ -126,6 +195,12 @@ export class Invite implements IInvite {
     })
   }
 
+  /**
+   * Fill in data
+   * @param {any} data 
+   * @param createdBy 
+   * @returns 
+   */
   static fill(data: any, createdBy?: User): Promise<Invite> {
     return new Promise<Invite>(async (resolve, _) => {
       var i = new this();
