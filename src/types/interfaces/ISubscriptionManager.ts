9f8e4a157eef7fd506d5bb20fe6869a5d568ec92
verlox <verlox@riseup.net>
2022-04-11 21:00:28 -0400

    finish SubscriptionManager interface

--- src/types/interfaces/ISubscriptionManager.ts
+++ src/types/interfaces/ISubscriptionManager.ts
@@ -1,3 +1,11 @@
-export interface ISubscriptionManager {
+import { Subscription } from '../Subscription';
+import { SubscriptionLevel } from '../SubscriptionLevel';
+import { User } from '../User';
 
+export interface ISubscriptionManager {
+  all:  Subscription[] | Subscription | null;
+
+  _getData(): Promise<void>;
+  subscribe(auth: User, subscriptionLevel: SubscriptionLevel, expiresAt?: Date, overwrite?: boolean): Promise<Subscription>;
+  unsubscribe(auth: User, subscription?: Subscription): Promise<void>;
 }
