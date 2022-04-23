287c779548b9eb29e894805f1323f0e3f0dd137c
verlox <verlox@riseup.net>
2022-04-23 00:09:02 -0400

    add .d.ts entrypoint

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+export { Core } from './dist/index.js';
+export { App } from './dist/types/App.js';
+export { User } from './dist/types/User.js';
+export { Variable } from './dist/types/Variable.js';
+export { Subscription } from './dist/types/Subscription.js';
+export { SubscriptionLevel } from './dist/types/SubscriptionLevel.js';
+export { File } from './dist/types/File.js';
+export { Invite } from './dist/types/Invite.js';
