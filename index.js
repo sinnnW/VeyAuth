f6a8faa94504c3a9cc04e3791faeaeb709a6305f
verlox <verlox@riseup.net>
2022-04-22 23:57:11 -0400

    create final export

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  Core: require('./dist/index.js'),
+  App: require('./dist/types/App.js'),
+  User: require('./dist/types/User.js'),
+  Variable: require('./dist/types/Variable.js'),
+  Subscription: require('./dist/types/Subscription.js'),
+  SubscriptionLevel: require('./dist/types/SubscriptionLevel.js'),
+  File: require('./dist/types/File.js'),
+  Invite: require('./dist/types/Invite.js')
+}
